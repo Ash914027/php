4386ab793644139f0c109fe4be46e64bd8c40814
Ashish Chauhan <119170180+Ash914027@users.noreply.github.com>
2024-08-28 11:14:00 +0530

    ..commit..

---
+++ ifelse.php
@@ -0,0 +1,21 @@
+
+<!-- if else statement in php -->
+<?php
+ $a=10;
+if($a>5){
+    echo "a is greater than 5\n";
+}
+else{
+    echo "a is less than 5\n";
+}
+?>
+<!--if age>18 then you can vote else you cannot vote-->
+<?php
+$age=20;
+if($age>18){
+    echo "You can vote\n";
+}
+else{
+    echo "You cannot vote\n";
+}
+?>
---
+++ phpfirst.php
@@ -0,0 +1,28 @@
+<?php
+  // Defining the variables
+  $text = "Hello, World!";
+  $num1 = 10;
+  $num2 = 20;
+
+  // Echoing the variables
+  echo $text."\n";
+  echo $num1."+".$num2."=";
+  echo $num1 + $num2;
+?>
+
+<!-- 
+
+// define("courses", [
+//   "PHP",
+//   "HTML",
+//   "CSS"
+// ]);
+// echo courses[0];
+// ?> -->
+
+
+
+
+
+
+
